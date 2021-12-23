749443ae61c7270ba888b3463c3253bda0393877
Tim Vercammen <83355491+timvercammen@users.noreply.github.com>
2021-12-23 13:25:37 +0100

    oef 3.2

--- oef4.1/stad.php
+++ oef4.1/stad.php
@@ -13,7 +13,7 @@ PrintJumbo();
 
         <?php
 
-        if ( ! is_numeric( $_GET['img_id']) ) die("Ongeldig argument " . $_GET['img_id'] . " opgegeven");
+        if ( !is_numeric( $_GET['img_id']) ) die("Ongeldig argument " . $_GET['img_id'] . " opgegeven");
 
         $rows = GetData( "select * from images where img_id=" . $_GET['img_id'] );
 
